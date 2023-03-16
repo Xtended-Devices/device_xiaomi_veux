2296d65a3ea21a2c0ea404f0dfb33cbd0b24063d
awaitz <aaron210987@gmail.com>
2023-03-16 14:43:49 +0000

    veux: Fix QPR1 compilation errors

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -204,6 +204,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2022-11-01
 
 # Sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
